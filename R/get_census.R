85a8b12423502b81c1f97b864213e164f01905ae
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2017-06-02 11:10:12 -0400

    added counties data.

--- R/get_census.R
+++ R/get_census.R
@@ -1,13 +1,23 @@
 #' Get census
 #' 
+#' This function takes an input lakescape, a user-defined land area around a 
+#' lake, and returns the total population for that area.
+#' 
 #' @param lakescape
+#' @param keep_output
 #' 
 #' @export
 #' @examples 
 #' get_census()
-get_census <- function(lake, lakescape, geo_data){
-  lakescape <- list()
-  return(lakescape)
+get_census <- function(lscape, keep_output){
+  
+  
+  
+  if(keep_output){
+    list(total_pop = total_pop,census = census)
+  } else {
+    total_pop
+  }
 }
 
 
