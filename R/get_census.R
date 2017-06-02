e5206e5ee66eef79ab6e856802a0680dccb23da3
Jeffrey W. Hollister <jeff.w.hollister@gmail.com>
2017-06-02 15:33:26 -0400

    still workinging on bg and lscape intersection.

--- R/get_census.R
+++ R/get_census.R
@@ -5,18 +5,60 @@
 #' 
 #' @param lakescape
 #' @param keep_output
+#' @param api_key
 #' 
 #' @export
 #' @examples 
 #' get_census()
-get_census <- function(lscape, keep_output){
+get_census <- function(lscape, keep_output, 
+                       api_key = Sys.getenv("CENSUS_API_KEY")){
+  
+ browser()
+  # Add in a check for census api key
+  
+  # Get total pop for each block group
+  data("counties_20m_sf")
+  if(sf::st_crs(counties_20m_sf) != sf::st_crs(lscape)){
+    county <- sf::st_transform(counties_20m_sf, sf::st_crs(lscape))
+  } else {
+    county <- counties_20m_sf
+  }
+  cnt_idx<-unlist(sf::st_intersects(lscape,county))
+  state_cnt <- data.frame(county[cnt_idx,][,c("STATEFP","COUNTYFP")])
+  state_cnt$STATEFP <- as.numeric(as.character(state_cnt$STATEFP))
+  state_cnt$COUNTYFP <- as.array(as.character(state_cnt$COUNTYFP))
+  tot_pop <- data.frame()
+  bg <- sf::st_as_sf(tigris::block_groups(state = state_cnt[1,1], 
+                                          county = state_cnt[1,2],2010))
+  bg <- bg[NULL,]
+  for(i in seq_along(state_cnt[,1])){
+    rin <- paste0("state:",state_cnt[i,1],"+county:",state_cnt[i,2])
+    tot_pop <- rbind(tot_pop,censusapi::getCensus(name = "sf1",vintage = 2010, 
+                         key = api_key,
+                         vars = "P0010001", region = "block group:*", 
+                         regionin = rin))
+    bg <- rbind(bg,sf::st_as_sf(tigris::block_groups(state = state_cnt[i,1], 
+                                            county = state_cnt[i,2],2010)))
+    
+  }
+  bg <- sf::st_transform(bg,sf::st_crs(lscape))
+  bg <- bg[unlist(sf::st_intersects(lscape,bg)),]
+  lscape_bg <- sf::st_intersection(lscape,bg))
+  #st_boundary plots 
+  #need to figure out how to get the BG ids back onto lscape_bg
+  #need to get density for BG need to get areas for bg in lscape and multiply by 
+  #bg density.
+  # Get Total area of block groups and total area of block groups inside of 
+  # lakescape
   
   
+  
+ 
   
   if(keep_output){
     list(total_pop = total_pop,census = census)
   } else {
-    total_pop
+    tot_pop
   }
 }
 
--- R/get_lakescape.R
+++ R/get_lakescape.R
@@ -13,6 +13,8 @@
 #' @param lake A valid \code{sf} object of the polygon(s) that make up the lake.
 #' @param lakescape A valid \code{sf} object of the user-defined landscape 
 #'                  surrounding the lake.
+#' @param id A 1x1 data frame containing the id and name.  Defualt is 
+#'           \code{data.frame(id = 1)}.
 #' @param data_src A character vector indicating which data source(s) to 
 #'                 summarize.  Valid entries include: "census", "lulc", or, 
 #'                 "impervious"
@@ -22,12 +24,23 @@
 #' 
 #' @export
 #' @examples 
-#' get_lakescape()
-get_lakescape <- function(lake, lakescape, 
+#' data(lake_sf, package = "lakescape")
+#' lakescape <- sf::st_buffer(lake_sf, 3000)
+#' get_lakescape(lake_sf, lakescape, data.frame(COMID = lakescape$COMID),
+#'               "census")
+get_lakescape <- function(lake, lakescape, id = data.frame(id = 1),
                           data_src = c("census", "lulc", "impervious"),
                           keep_output = F, ...){
-  lscape <- sf::st_difference(lakescape, lake)
+  if(is.na(st_crs(lakescape)) | is.na(st_crs(lake))){
+    stop(paste("A valid coordinate reference system is required for lake and 
+               lakescape.  In the meantime, enjoy this dad joke:\n", dadjoke::groan())
+  }
   
+  lscape <- sf::st_difference(st_geometry(lakescape), st_geometry(lake))
+  #add id - need to find better way than df...
+  if(!is.null(id)){
+    lscape <- st_as_sf(cbind(id,lscape))
+  }
   for(i in data_src){
     if(i == "census"){
       census <- get_census(lscape, keep_output)
